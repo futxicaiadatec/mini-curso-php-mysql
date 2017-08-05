2214d737525932ff6406d5df0cc83075e5101eea
Jivago Medeiros <Jivago@ic.ufmt.br>
2017-08-05 14:33:08 -0400

    adiciona exemplo de enviado de dados em formulário para arquivo PHP

---
+++ Exemplos/recebe-dados.php
@@ -0,0 +1,13 @@
+<?php
+
+	//print "Oi";
+	
+	//echo $arr;
+	
+	print_r($_POST);
+
+?>
+<p>
+	<strong>Dados Recebidos pelo Formulário</strong>
+	<br />Nome completo: <?php echo $_POST['nome'] ?>
+</p>
