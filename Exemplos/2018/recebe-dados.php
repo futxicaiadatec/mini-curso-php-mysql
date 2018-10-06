ad01ac3fc89fcdf3e7c5357fc9fac4b8b1850190
Jivago Medeiros <jivago@ic.ufmt.br>
2018-10-06 14:48:28 -0400

    Exemplo de formulários com PHP

---
+++ Exemplos/2018/recebe-dados.php
@@ -0,0 +1,34 @@
+
+
+
+
+<?php
+
+	//print_r($_POST);
+	//$_REQUEST
+
+	$nome = $_POST['txt-nome'];
+	$rga  = $_POST['txt-rga'];
+	
+	print_r($_GET);
+	
+	
+?>
+
+<p>
+	<strong>Seu nome:</strong>
+	<?php echo $nome ?>	
+	<br />
+	<strong>Seu RGA:</strong> 
+	<?php echo $rga ?>	<br />
+	<strong>Cidade:</strong> 
+	<?php echo $_POST['cidade'] ?>
+	<br />	
+	<strong>Interesses:</strong> 
+	<?php 
+foreach ($_POST['interesses'] as $interesse) {
+	echo "<br />".$interesse;
+}
+	?>
+	<br />
+</p>
