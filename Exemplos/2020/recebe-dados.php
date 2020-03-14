3a5d59fe084aead688e88593fcebfaa595ece445
Jivago Medeiros <jivago@ic.ufmt.br>
2020-03-14 16:19:14 -0400

    Enviando os dados para o banco de dados

--- Exemplos/2020/recebe-dados.php
+++ Exemplos/2020/recebe-dados.php
@@ -5,7 +5,23 @@
 
 echo "<br />Nome: ".$_REQUEST['nome'];
 echo "<br />Raça: ".$_REQUEST['raca'];
-	
 
+$nome = $_REQUEST['nome'];
+$raca = $_REQUEST['raca'];	
+	//INSERT INTO gatinhos (nome,raca)
+	//VALUES ('Berinjela','Vira Lata');
+	
+$con = mysqli_connect("localhost", "root", "");
+mysqli_select_db($con, "futxicaiada");
+
+$str_insert = 
+ "INSERT INTO gatinhos (nome,raca)
+	VALUES ('".$nome."','".$raca."')";
+
+mysqli_query($con,$str_insert);
+
+echo "<p>
+		Gatinho inserido com sucesso!!!
+	 </p>";
 
 ?>
